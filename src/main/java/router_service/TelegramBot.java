041a57495ab8d50c5cf482f85122046afc4d7e91
rilialov <ilialovrustem@gmail.com>
2022-05-04 16:52:56 +0300

    Initial commit

---
+++ src/main/java/router_service/TelegramBot.java
@@ -0,0 +1,21 @@
+package router_service;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class TelegramBot extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+}
