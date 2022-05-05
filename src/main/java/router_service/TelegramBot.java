e7631696b0e4c71719f78ad83ebe6e1bd708cfa5
rilialov <ilialovrustem@gmail.com>
2022-05-05 14:27:26 +0300

    Added TelegramBot

--- src/main/java/router_service/TelegramBot.java
+++ src/main/java/router_service/TelegramBot.java
@@ -1,21 +1,62 @@
 package router_service;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.io.*;
+import java.util.Properties;
 
 public class TelegramBot extends TelegramLongPollingBot {
+
+    private String botName;
+    private String botToken;
+
+    {
+        loadProperties();
+    }
+
     @Override
     public String getBotUsername() {
-        return null;
+        return botName;
     }
 
     @Override
     public String getBotToken() {
-        return null;
+        return botToken;
     }
 
     @Override
     public void onUpdateReceived(Update update) {
+        if(update.hasMessage() && update.getMessage().hasText()) {
+            String message = update.getMessage().getText().trim();
+            String chatId = update.getMessage().getChatId().toString();
 
+            SendMessage sm = new SendMessage();
+            sm.setChatId(chatId);
+            sm.setText(message);
+
+            try {
+                execute(sm);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void loadProperties() {
+        String USER_DIR = System.getProperty("user.dir");
+        File file = new File(USER_DIR + "\\application.properties");
+        Properties properties = new Properties();
+        try {
+            FileReader fileReader = new FileReader(file);
+            properties.load(fileReader);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+        botName = properties.getProperty("botName");
+        botToken = properties.getProperty("botToken");
     }
 }
